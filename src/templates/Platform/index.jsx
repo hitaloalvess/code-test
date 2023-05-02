9fcc0e2aa2ac7ab3bfe202e80ad6d4ed2b6acbc5
hitaloalvess <hitalo.ralves@outlook.com>
2023-05-02 10:06:08 -0300

    feat: add header in plataform template

--- src/templates/Platform/index.jsx
+++ src/templates/Platform/index.jsx
@@ -6,11 +6,13 @@ import { HTML5Backend } from 'react-dnd-html5-backend';
 import { TouchBackend } from 'react-dnd-touch-backend';
 
 import { isMobile } from 'react-device-detect';
+import Header from '../../components/Header';
 
 
 const Platform = () => (
     <DndProvider backend={isMobile ? TouchBackend : HTML5Backend}>
         <main className={container}>
+            <Header />
         </main>
     </DndProvider>
 )
