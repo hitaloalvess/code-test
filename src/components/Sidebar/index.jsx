6326e28825ea8dfc977a565096ca19c9ac119a68
hitaloalvess <hitalo.ralves@outlook.com>
2023-05-06 01:17:17 -0300

    feat: add confirmation modal for device deletion from sidebar

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,18 +1,30 @@
 import { useState } from 'react';
 import { useDrop } from 'react-dnd';
 
+import { useModal } from '@/hooks/useModal';
 import SidebarArea from './SidebarArea';
 import MenuButtons from './MenuButtons';
 
 import { menu } from './styles.module.css';
 
 const Sidebar = ({ deleteDevice }) => {
-
+    const { enableModal, disableModal } = useModal();
     const [currentArea, setCurrentArea] = useState('entry');
 
+    const handleDeleteDevice = (id) => {
+        enableModal({
+            typeContent: 'confirmation',
+            title: 'Cuidado',
+            subtitle: 'Tem certeza que deseja excluir o componente?',
+            handleConfirm: () => {
+                deleteDevice(id);
+                disableModal();
+            }
+        })
+    }
     const [{ isOver }, drop] = useDrop(() => ({
         accept: 'device',
-        drop: (item) => deleteDevice(item.id),
+        drop: (item) => handleDeleteDevice(item.id),
         collect: (monitor) => ({
             isOver: !!monitor.isOver()
         })
