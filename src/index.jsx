5b63d9fe38219ca24587aea15287544dccc6a8d0
hitaloalvess <hitalo.ralves@outlook.com>
2023-07-12 11:02:18 -0300

    feat: add react router dom

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 import Modal from 'react-modal';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
 
 import Platform from './templates/Platform';
+import SignIn from './templates/Signin';
+
 import './styles/global.css';
 
 Modal.setAppElement('#root');
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Platform />
+    <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<SignIn />} />
+      <Route path='/platform' element={<Platform />} />
+    </Routes>
+   </BrowserRouter>
   </React.StrictMode>,
 )
